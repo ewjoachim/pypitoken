9cd8fbd48e26afb4a11c606739b73bc240ec0f4e
Joachim Jablon <ewjoachim@gmail.com>
2021-03-14 10:47:42 +0100

    Document exception classes

--- pypitoken/exceptions.py
+++ pypitoken/exceptions.py
@@ -1,14 +1,32 @@
 class PyPITokenException(Exception):
-    pass
+    """
+    The base exception for all exceptions raised
+    from this library. Any code raising a different
+    exception would be considered a bug.
+    """
 
 
 class LoadError(PyPITokenException):
-    pass
+    """
+    Exception encoutered while calling `Token.load`, due to unexpected
+    format.
+    """
 
 
 class ValidationError(PyPITokenException):
-    pass
+    """
+    Exception encoutered while calling `Token.check`, the token should
+    be considered invalid.
+    """
 
 
 class MissingContextError(ValidationError, NotImplementedError):
-    pass
+    """
+    Token check failed du to missing information in the passed context.
+
+    The token should be considered invalid, but this is likely a problem in the
+    implementing code: we most likely forgot a context element.
+
+    Due to PyMacaroons & this lib implementation details, this exception is currently
+    not fired by `Token.check`.
+    """
